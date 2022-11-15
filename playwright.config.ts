2e69ad021a037fef4683a0ca56312daa2e53c466
Jacob Ebey <jacob.ebey@live.com>
2022-11-14 18:17:06 -0800

    updated playwright workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,9 @@ const config: PlaywrightTestConfig = {
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: process.env.CI ? "github" : [["html", { open: "on-failure" }]],
+	reporter: process.env.CI
+		? [["github"], ["html", { open: "never" }]]
+		: [["html", { open: "on-failure" }]],
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
